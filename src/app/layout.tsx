74b93e5a5051ca9960fec49a9a560f8fc2546e4a
Sarthak <jainsarthak2003@gmail.com>
2024-12-27 14:15:02 +0530

    added toastify

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import AuthProvider from "@/context/AuthProvider";
+import { ToastContainer, Bounce } from "react-toastify";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,10 +36,23 @@ export default function RootLayout({
             {children}
             <footer className="bg-gray-100 py-6 w-full">
               <div className="container mx-auto px-4 text-center text-gray-600">
-                <p>&copy; 2025 Social Analytics. All rights reserved.</p>
+                <p>Copyright &copy; 2025 Socialytics. All rights reserved.</p>
               </div>
             </footer>
           </div>
+          <ToastContainer
+            position="bottom-right"
+            autoClose={3000}
+            hideProgressBar
+            newestOnTop
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="colored"
+            transition={Bounce}
+          />
         </body>
       </AuthProvider>
     </html>
