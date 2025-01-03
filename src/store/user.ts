8c628faf0cba17c6b825b9d3de108b1ce40ea12d
Sarthak <jainsarthak2003@gmail.com>
2025-01-03 20:13:49 +0530

    added zustand and user store

---
+++ src/store/user.ts
@@ -0,0 +1,50 @@
+import { create } from "zustand";
+import { immer } from "zustand/middleware/immer";
+import { persist } from "zustand/middleware";
+
+interface UserStore {
+  name: string | null;
+  email: string | null;
+  isXConnected: boolean;
+  isInstagramConnected: boolean;
+  isLinkedinConnected: boolean;
+  isYoutubeConnected: boolean;
+  hydrated: boolean;
+
+  setHydrated: () => void;
+  setUser: (updates: Partial<UserStore>) => void;
+}
+
+export const useUserStore = create<UserStore>()(
+  persist(
+    immer((set) => ({
+      name: null,
+      email: null,
+      hydrated: false,
+      isXConnected: false,
+      isInstagramConnected: false,
+      isLinkedinConnected: false,
+      isYoutubeConnected: false,
+      setHydrated: () => {
+        set((state) => {
+          state.hydrated = true;
+        });
+      },
+      setUser: (updates: Partial<UserStore>) => {
+        set((state) => {
+          Object.entries(updates).forEach(([key, value]) => {
+            (state as any)[key] = value ?? (state as any)[key];
+          });
+        });
+      },
+    })),
+    {
+      name: "user",
+      onRehydrateStorage() {
+        return (state, error) => {
+          if (!error) state?.setHydrated();
+        };
+      },
+    }
+  )
+);
