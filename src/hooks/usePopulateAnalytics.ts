c8abcd130ee8d60341364e155cc627eb3e7c1ec6
Sarthak <jainsarthak2003@gmail.com>
2025-01-10 15:03:31 +0530

    removed lastUpdate from custom hook for getting the data from db and populating the store

--- src/hooks/usePopulateAnalytics.ts
+++ src/hooks/usePopulateAnalytics.ts
@@ -7,38 +7,42 @@ import { useUserStore } from "@/store/user";
 import { useAnalyticsStore } from "@/store/analytics";
 
 export function usePopulateAnalytics() {
-    const { data: session } = useSession();
-    const [loading, setLoading] = useState(true);
-    const [xChartData, setXChartData] = useState<TwitterChartData | null>(null);
-    const { isXConnected, isInstagramConnected, isLinkedinConnected, isYoutubeConnected, email } = useUserStore();
-    const { setAnalytics, lastUpdate, xData } = useAnalyticsStore();
+  const { data: session } = useSession();
+  const [loading, setLoading] = useState(true);
+  const [xChartData, setXChartData] = useState<TwitterChartData | null>(null);
+  const {
+    isXConnected,
+    isInstagramConnected,
+    isLinkedinConnected,
+    isYoutubeConnected,
+    email,
+  } = useUserStore();
+  const { setAnalytics, xData } = useAnalyticsStore();
 
-    const populateAnalytics = async () => {
-        if (lastUpdate === null || Date.now() - Number(lastUpdate) >= 86400000) {
-            setLoading(true);
-            let currentXData: TwitterChartData | null = xData ? xData : null;
+  const populateAnalytics = async () => {
+    setLoading(true);
+    let currentXData: TwitterChartData | null = xData ? xData : null;
 
-            if (isXConnected && currentXData === null) {
-                const xDataFromDB = await getXUserData(email as string);
-                if (xDataFromDB?.chartsData) {
-                    currentXData = xDataFromDB.chartsData;
-                }
-            }
+    if (isXConnected && currentXData === null) {
+      const xDataFromDB = await getXUserData(email as string);
+      if (xDataFromDB?.chartsData) {
+        currentXData = xDataFromDB.chartsData;
+      }
+    }
 
-            setXChartData(currentXData);
+    setXChartData(currentXData);
 
-            if (setAnalytics) {
-                setAnalytics({ xData: currentXData, lastUpdate: new Date() });
-            }
-        }
-        setLoading(false);
-    };
+    if (setAnalytics) {
+      setAnalytics({ xData: currentXData, lastUpdate: new Date() });
+    }
+    setLoading(false);
+  };
 
-    useEffect(() => {
-        if (session?.user && email === session.user.email) {
-            populateAnalytics();
-        }
-    }, [session, email]);
+  useEffect(() => {
+    if (session?.user && email === session.user.email) {
+      populateAnalytics();
+    }
+  }, [session, email]);
 
-    return { loading, xChartData };
+  return { loading, xChartData };
 }
